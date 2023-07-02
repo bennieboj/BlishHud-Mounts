7686102a8a061ded7163a17d2d155de6329084b9
Bennieboj <4215739+bennieboj@users.noreply.github.com>
2023-07-02 20:11:26 +0200

    fix dismounting

--- Module.cs
+++ Module.cs
@@ -614,7 +614,7 @@ namespace Manlaan.Mounts
             Logger.Debug("DoDefaultMountActionAsync entered");
             if (GameService.Gw2Mumble.PlayerCharacter.CurrentMount != MountType.None && IsMountSwitchable())
             {
-                await (_helper.GetLastUsedMount()?.DoUnmountAction() ?? Task.CompletedTask);
+                await (_helper.GetCurrentlyActiveMount()?.DoUnmountAction() ?? Task.CompletedTask);
                 Logger.Debug("DoDefaultMountActionAsync dismounted");
                 return;
             }
