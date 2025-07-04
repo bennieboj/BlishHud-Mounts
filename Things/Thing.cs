d6c7a8dad1b67c3a2d70999c10de1511a01c6109
Bennieboj <4215739+bennieboj@users.noreply.github.com>
2025-07-04 22:28:12 +0200

    fix

--- Things/Thing.cs
+++ Things/Thing.cs
@@ -102,7 +102,6 @@ namespace Manlaan.Mounts.Things
                 else
                 {
                     QueueAfterFalling();
-                    return;
                 }
             }
 
